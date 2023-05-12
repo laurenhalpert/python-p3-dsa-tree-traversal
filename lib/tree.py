e2dbba5c1cb57bf9c2eb8c7db4a1d1af89189ef7
Lauren <halpertln@gmail.com>
2023-05-12 14:44:55 -0600

    complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,11 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) >0:
+      current = nodes_to_visit.pop()
+      if current['id'] == id:
+        return current
+      nodes_to_visit = nodes_to_visit + current['children']
+    return None
